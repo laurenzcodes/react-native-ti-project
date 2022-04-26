4015a24b47910e9024728e5c7dee605eb666e6e7
Robert Marquardt <r.marquardt@clevershuttle.de>
2022-04-26 14:42:19 +0200

    feat: set up react native bootstrap project

--- test.tsx
+++ test.tsx
@@ -1,19 +1,19 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable no-var */
-/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 
-import { Text, View } from 'react-native';
+import {Text, View} from 'react-native';
 import styled from 'styled-components/native';
 
 const TestComponent: React.FC<{
   data: any;
-}> = (props) => {
+}> = props => {
   var count = 0;
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{flex: 1}}>
       <Text style={componentStyles.text}>{count}</Text>
-      <IncreaseCounter onPress={() => count++}>Increase counter</IncreaseCounter>
+      <IncreaseCounter onPress={() => count++}>
+        Increase counter
+      </IncreaseCounter>
       <Text>{props.data.text}</Text>
     </View>
   );
